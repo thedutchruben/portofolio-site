fc57b746bbbd2230d81982311e4c62b0a484a0ff
Ruben de Roos <deroosr@gmail.com>
2020-06-15 14:01:49 +0200

    Remove Comments
    remove comments lines

--- include/admin/UploadImage.php
+++ include/admin/UploadImage.php
@@ -5,49 +5,36 @@ function image($last_id){
     $target_file = $target_dir .$last_id. basename($_FILES["fileToUpload"]["name"]);
     $uploadOk = 1;
     $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// Check if image file is a actual image or fake image
     if(isset($_POST["CreatePage"])) {
         $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
         if($check !== false) {
-//        echo "File is an image - " . $check["mime"] . ".";
             $uploadOk = 1;
         } else {
-//        echo "File is not an image.";
+            //    todo Throw error that it isnt a image
+
             $uploadOk = 0;
         }
     }
 
-// Check if file already exists
     if (file_exists($target_file)) {
-//    echo "Sorry, file already exists.";
-//    echo $_FILES["fileToUpload"]["name"];
-//    echo $target_file;
         $uploadOk = 0;
     }
 
-// Check file size
     if ($_FILES["fileToUpload"]["size"] > 500000) {
-//    echo "Sorry, your file is too large.";
         $uploadOk = 0;
     }
 
-// Allow certain file formats
     if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
         && $imageFileType != "gif" ) {
-//    echo $imageFileType;
-//    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
         $uploadOk = 0;
     }
 
-// Check if $uploadOk is set to 0 by an error
+
     if ($uploadOk == 0) {
-//    echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
+        //    todo Throw error that image isnt uploaded
     } else {
         if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-//        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-        } else {
-//        echo "Sorry, there was an error uploading your file.";
+
         }
 
 
